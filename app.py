ae7cb042e85fb663584916a12a76ba431c4d6a47
AlikaKis <42034121+AlikaKis@users.noreply.github.com>
2021-11-17 19:30:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,23 +5,9 @@ app = Flask(__name__)
 @app.route('/getmsg/', methods=['GET'])
 def respond():
     # Retrieve the name from url parameter
-    with open('my_pics.jpg','wb') as target:
-        a = requests.post('https://diddlybop.ru/wp-content/uploads/2016/10/fotografii-dikoy-prirody_39.jpg')
-        target.write(a.content)
+    response = requests.get('https://api.github.com')
+    response.text
 
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
     return jsonify(response)
 
 @app.route('/post/', methods=['POST'])
